cbf99f0133847c25ed6c7c61ffddbc038cf0f46d
fake-or-dead <a.postzone@gmail.com>
2016-01-25 14:00:05 +0700

    replace tab with space, set 2 space for tab, alignment php structure

--- config/admin.php
+++ config/admin.php
@@ -1,33 +1,29 @@
 <?php
 return [
 
-	'defaultLanguage' 		=> 'th' ,
-	'listTransLang'			=> ['th','vn'],
-	
-	'cachetime_shorturl'	=> 432000 ,
-	'cache'				=> [ 
-							'election'			=> '300' ,   //5 minute	
-							'queryOther'		=> '1' , //1 minute
-							'queryLoadMore'		=> '1' //1 minute
-							],
+  'defaultLanguage' => 'th' ,
+  'listTransLang'   => ['th','vn'],
 
-	'upload' 				=> [
-								'popup' => [
-											'path' 		=> 'upload/popup/',
-											'detail' 	=> [
-															'80x60'
-														],
-											'file' 		=> [
-															'80x60' => [
-																		'size' 		=> '80,59-61',
-																		'storage'	=> 1024,
-																		'type' 		=> 'jpeg,gif,png'
-																		]
-													]
-
-
-											]
-							],
-	'defultRecord' 			=> 4 ,
-
-	];
+  'cachetime_shorturl' => 432000 ,
+  'cache' =>  [
+                'election'      => '300' ,  //5 minute
+                'queryOther'    => '1' ,    //1 minute
+                'queryLoadMore' => '1'      //1 minute
+              ],
+  'upload' => [
+              'popup' => [
+                'path'    => 'upload/popup/',
+                'detail'  => [
+                  '80x60'
+                ],
+                'file' => [
+                    '80x60' => [
+                      'size'    => '80,59-61',
+                      'storage' => 1024,
+                      'type'    => 'jpeg,gif,png'
+                    ]
+                  ]
+                ]
+              ],
+  'defultRecord' => 4
+];
