4ecf3dea20451bb7225941799e750aec8519b34e
GolfNa <pakaya16@gmail.com>
2016-01-26 14:39:41 +0700

    update shop of the week

---
+++ app/Http/Controllers/Admin/ShopOfTheWeekController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Requests\Admin\ShopOfTheWeekRequest;
+use App\Http\Controllers\Controller;
+use App\Model\Admin\ShopOfTheWeek;
+use View,Config;
+
+class ShopOfTheWeekController extends Controller
+{
+    /**
+    * DateCreate 2016-01-25
+    * Create By golf
+    */
+
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index(Request $request)
+    {
+      if ($request->has('q'))
+      {
+        $search                 = e($request->input('q')) ;
+        $setData['data']        = ShopOfTheWeek::orWhere('id', 'LIKE', '%'.$search.'%')
+                                  ->orWhere('group_name', 'LIKE', '%'.$search.'%')
+                                  ->orderBy('id', 'desc')
+                                  ->paginate(Config::get('admin.defultRecord'));
+
+        $setData['pagination']  = $setData['data']->appends(['q' => $request->input('q')])->links() ;
+        $setData['search']      = $request->input('q') ;
+      }
+      else
+      {
+        $setData['data']        = ShopOfTheWeek::with('user')->orderBy('id' , 'desc')
+                                  ->paginate(Config::get('admin.defultRecord'));
+
+        $setData['pagination']  = $setData['data']->links() ;
+      }
+
+      return View::make('admin.shopoftheweek.index', $setData) ;
+    }
+
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function create()
+    {
+      $setData['actionLink']  = action('Admin\ShopOfTheWeekController@store') ;
+      $setData['action']      = 'create' ;
+      $setData['data']        = [];
+
+      return View::make('admin.shopoftheweek.add_edit', $setData) ;
+    }
+
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  App\Http\Requests\Admin\ShopOfTheWeekRequest  $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(ShopOfTheWeekRequest $request)
+    {
+      $nameImage                  = 'ShopOfTheWeek-'.date('YmdHis').'.'.$request->file('imageupload')->guessExtension() ;
+      $request->file('imageupload')->move(Config('admin.upload.admin.path'), $nameImage);
+
+      $dataInsert                 = $request->except(['_token','imageupload']) ;
+      $dataInsert['user_id']      = $request->session()->get('backoffice')['id'] ;
+      $dataInsert['image_url']    = $nameImage ;
+
+      ShopOfTheWeek::create(beforeSql($dataInsert));
+      return redirect()->action('Admin\ShopOfTheWeekController@index');
+    }
+
+    /**
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function show(int $id)
+    {
+        //
+    }
+
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function edit(int $id)
+    {
+      $setData['data']                = ShopOfTheWeek::where('id', $id)->first() ;
+      $setData['actionLink']          = action('Admin\ShopOfTheWeekController@update',['id'=>$id]) ;
+
+      if ($setData['data'])
+      {
+        $setData['action']            = 'edit' ;
+        return View::make('admin.shopoftheweek.add_edit', $setData) ;
+      }
+      else
+      {
+        return abort(404);
+      }
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  App\Http\Requests\Admin\ShopOfTheWeekRequest  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function update(ShopOfTheWeekRequest $request, int $id)
+    {
+        //
+    }
+
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy(int $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/Admin/ShopOfTheWeekRequest.php
@@ -0,0 +1,93 @@
+<?php
+namespace App\Http\Requests\Admin;
+
+use App\Http\Requests\Request;
+use App\Model\Admin\ShopOfTheWeek;
+use Config;
+
+class ShopOfTheWeekRequest extends Request
+{
+  /**
+  * DateCreate 2016-01-25
+  * Create By golf
+  */
+  private static $width ;
+  private static $height ;
+  /**
+  * Determine if the user is authorized to make this request.
+  *
+  * @return bool
+  */
+  public function authorize()
+  {
+    return true;
+  }
+
+  /**
+  * Get the validation rules that apply to the request.
+  *
+  * @return array
+  */
+  public function rules()
+  {
+    switch($this->method())
+    {
+      case 'GET':
+      case 'DELETE':
+      case 'POST':
+      {
+        self::setSize();
+        return [
+                'name'        => 'required|alpha_dash',
+                'link_url'    => 'required|alpha_dash',
+                'status'      => ['required','integer'],
+                // 'imageupload' => 'required|image|image_size:'.self::$width.','.self::$height.'|mimes:'.Config::get('admin.upload.admin.mimes').'|max:'.Config::get('admin.upload.admin.storage'),
+        ];
+      }
+      case 'PUT':
+      case 'PATCH':
+      {
+        return [];
+      }
+      default:break;
+    }
+  }
+
+  /**
+  * Change Language from user choose.
+  *
+  * @return array
+  */
+  public function messages()
+  {
+    if (in_array($this->session()->get('lang'), Config('admin.listTransLang')))
+    {
+      return [
+        'name.required'           => trans('admin/text_message.nameShopOfTheWeek') . ' ' . trans('error.required'),
+        'name.alpha_dash'         => trans('admin/text_message.nameShopOfTheWeek') . ' ' . trans('error.alpha_dash'),
+        'link_url.required'       => trans('admin/text_message.link_url') . ' ' . trans('error.required'),
+        'limit.alpha_dash'        => trans('admin/text_message.link_url') . ' ' . trans('error.alpha_dash'),
+        'imageupload.required'    => trans('admin/text_message.imageupload') . ' ' . trans('error.required'),
+        'imageupload.image'       => trans('error.image'),
+        'imageupload.image_size'  => trans('admin/text_message.imageupload') . ' ' . trans('error.image_size'),
+        'imageupload.mimes'       => trans('admin/text_message.imageupload') . ' ' . trans('error.mimes'),
+        'imageupload.max'         => trans('admin/text_message.imageupload') . ' ' . trans('error.imageSizeMax'),
+        'status.required'         => trans('admin/text_message.status') . ' ' . trans('error.required'),
+        'status.integer'          => trans('admin/text_message.status') . ' ' . trans('error.integer'),
+      ];
+    }
+
+    return [] ;
+  }
+
+  /**
+  * Change Language from user choose.
+  *
+  */
+  private static function setSize()
+  {
+    $dataSelect     = ShopOfTheWeek::getLocation()->first();
+    self::$width    = $dataSelect->width;
+    self::$height   = $dataSelect->height;
+  }
+}
